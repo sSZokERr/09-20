542afc076109c5a8215a8a8ca231211a615eb97d
szoker <MY_NAME@example.com>
2022-09-20 15:21:08 +0200

    szamol()

--- main.js
+++ main.js
@@ -15,7 +15,17 @@ function folotte(){
 
 document.getElementById('bekezdes').addEventListener('pointerover', folotte)
 
+function szamol(){
+    let egyikszam = document.getElementById('egyik').value;
+    let masikszam = document.getElementById('masik').value;
+    document.getElementById('ered').value = (parseFloat(egyikszam) + parseFloat(masikszam));
+}
+
+
+
 function init(){
+    document.getElementById('masik').addEventListener('change', szamol)
+    document.getElementById('egyik').addEventListener('change', szamol)
     console.log('init')
 }
 
