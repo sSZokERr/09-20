527e3306c25ba8e6d56ce02934c62cea8fbaf6cf
szoker <MY_NAME@example.com>
2022-09-20 15:00:19 +0200

    pointerover

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 function katt(){
     window.alert('Katt')
 }
+
 function duppla(){
     window.alert('katt katt')
 }
+
 document.getElementById('fogomb').addEventListener('click', katt)
 document.getElementById('fogomb').addEventListener('dblclick', duppla)
+
+function folotte(){
+    console.log('pointer')
+}
+
+document.getElementById('bekezdes').addEventListener('pointerover', folotte)
