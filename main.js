c4e6daca2b11f2f274b73970a245b71bd640deeb
szoker <MY_NAME@example.com>
2022-09-20 14:58:39 +0200

    double

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function katt(){
     window.alert('Katt')
 }
+function duppla(){
+    window.alert('katt katt')
+}
 document.getElementById('fogomb').addEventListener('click', katt)
+document.getElementById('fogomb').addEventListener('dblclick', duppla)
